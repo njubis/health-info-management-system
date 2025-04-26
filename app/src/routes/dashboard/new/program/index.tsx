0396cb6d6f014ca6ede5463aaf2e239a27c1a3a1
Njubi  Simon <njubi.dev@gmail.com>
2025-04-26 20:08:38 +0300

    fix(app): removed cloudflare pages version of the app

--- app/src/routes/dashboard/new/program/index.tsx
+++
@@ -1,6 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-import { ProgramForm } from "~/components/ProgramForm/ProgramForm";
-
-export default component$(() => {
-  return <ProgramForm />;
-});
